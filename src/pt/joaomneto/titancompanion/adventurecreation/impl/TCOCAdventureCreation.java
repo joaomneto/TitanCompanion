3205fe9dbecb079a1653c7bef2fa790e55eadb44
Joao Neto <joao.m.neto@gmail.com>
2017-05-03 21:00:51 +0100

    Changes to Citadel of Chaos adventure creation

--- src/pt/joaomneto/titancompanion/adventurecreation/impl/TCOCAdventureCreation.java
+++ src/pt/joaomneto/titancompanion/adventurecreation/impl/TCOCAdventureCreation.java
@@ -3,7 +3,10 @@ package pt.joaomneto.titancompanion.adventurecreation.impl;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import pt.joaomneto.titancompanion.R;
 import pt.joaomneto.titancompanion.adventure.Adventure.AdventureFragmentRunner;
@@ -14,77 +17,117 @@ import android.view.View;
 
 public class TCOCAdventureCreation extends AdventureCreation {
 
-	private final static int FRAGMENT_TCOC_SPELLS = 1;
+    private final static int FRAGMENT_TCOC_SPELLS = 1;
 
-	private int spellValue = -1;
-	List<String> spells = new ArrayList<String>();
-	
-	public TCOCAdventureCreation() {
-		super();
-		fragmentConfiguration.clear();
-		fragmentConfiguration.put(0, new AdventureFragmentRunner(
-				R.string.title_adventure_creation_vitalstats,
-				"pt.joaomneto.titancompanion.adventurecreation.impl.fragments.VitalStatisticsFragment"));
-		fragmentConfiguration.put(FRAGMENT_TCOC_SPELLS, new AdventureFragmentRunner(
-				R.string.spells,
-				"pt.joaomneto.titancompanion.adventurecreation.impl.fragments.tcoc.TCOCAdventureCreationSpellsFragment"));
+    private int spellValue = -1;
+    Map<String, Integer> spells = new HashMap<>();
+    List<String> spellList = new ArrayList<>();
 
-	}
+    public TCOCAdventureCreation() {
+        super();
+        fragmentConfiguration.clear();
+        fragmentConfiguration.put(0, new AdventureFragmentRunner(
+                R.string.title_adventure_creation_vitalstats,
+                "pt.joaomneto.titancompanion.adventurecreation.impl.fragments.VitalStatisticsFragment"));
+        fragmentConfiguration.put(FRAGMENT_TCOC_SPELLS, new AdventureFragmentRunner(
+                R.string.spells,
+                "pt.joaomneto.titancompanion.adventurecreation.impl.fragments.tcoc.TCOCAdventureCreationSpellsFragment"));
 
-	@Override
-	protected void storeAdventureSpecificValuesInFile(BufferedWriter bw)
-			throws IOException {
-		
-		String spellsS = "";
+    }
 
-		if (!spells.isEmpty()) {
-			for (String spell : spells) {
-				spellsS += spell + "#";
-			}
-			spellsS = spellsS.substring(0, spellsS.length() - 1);
-		}
+    @Override
+    protected void storeAdventureSpecificValuesInFile(BufferedWriter bw)
+            throws IOException {
 
-		bw.write("spellValue="+spellValue+"\n");
-		bw.write("spells="+spellsS+"\n");
-		bw.write("gold=0\n");
-	}
+        String spellsS = "";
 
-	@Override
-	public String validateCreationSpecificParameters() {
-		StringBuilder sb = new StringBuilder();
-		boolean error = false;
-		if(this.spellValue < 0){
-			sb.append("Spell Count");
-			error = true;
-		}
-		sb.append(error?"; ":"");
-		if(this.spells == null || this.spells.isEmpty()){
-			sb.append("Chosen Spells");
-		}
+        if (!spells.isEmpty()) {
+            for (String spell : getSpellList()) {
+                spellsS += spell + "§"+spells.get(spell)+"#";
+            }
+            spellsS = spellsS.substring(0, spellsS.length() - 1);
+        }
 
-		return  sb.toString();
-	}
+        bw.write("spellValue="+spellValue+"\n");
+        bw.write("spells="+spellsS+"\n");
+        bw.write("gold=0\n");
+    }
 
-	private TCOCAdventureCreationSpellsFragment getTCOCSpellsFragment() {
-		TCOCAdventureCreationSpellsFragment tcocSpellsFragment = (TCOCAdventureCreationSpellsFragment) getFragments().get(FRAGMENT_TCOC_SPELLS);
-		return tcocSpellsFragment;
-	}
+    @Override
+    public String validateCreationSpecificParameters() {
+        StringBuilder sb = new StringBuilder();
+        boolean error = false;
+        if(this.spellValue < 0){
+            sb.append("Spell Count");
+            error = true;
+        }
+        sb.append(error?"; ":"");
+        if(this.spells == null || this.spells.isEmpty()){
+            sb.append("Chosen Spells");
+        }
 
-	@Override
-	protected void rollGamebookSpecificStats(View view) {
-		spellValue = DiceRoller.roll2D6().getSum()+6;
-		getTCOCSpellsFragment().getSpellScoreValue().setText(""+spellValue);
-		
-	}
+        return  sb.toString();
+    }
 
-	public synchronized int getSpellValue() {
-		return spellValue;
-	}
+    private TCOCAdventureCreationSpellsFragment getTCOCSpellsFragment() {
+        TCOCAdventureCreationSpellsFragment tcocSpellsFragment = (TCOCAdventureCreationSpellsFragment) getFragments().get(FRAGMENT_TCOC_SPELLS);
+        return tcocSpellsFragment;
+    }
 
-	public synchronized List<String> getSpells() {
-		return spells;
-	}
-	
-	
+    @Override
+    protected void rollGamebookSpecificStats(View view) {
+        spellValue = DiceRoller.roll2D6().getSum()+6;
+        getTCOCSpellsFragment().getSpellScoreValue().setText(""+spellValue);
 
+    }
+
+    public synchronized int getSpellValue() {
+        return spellValue;
+    }
+
+    public synchronized List<String> getSpellList() {
+        for(String spell: spells.keySet()){
+            if(!spellList.contains(spell)){
+                spellList.add(spell);
+            }
+        }
+
+        Collections.sort(spellList);
+        return spellList;
+    }
+
+
+    public void addSpell(String spell) {
+        if(!spells.containsKey(spell)){
+            spells.put(spell, 1);
+        }
+        spells.put(spell, spells.get(spell)+1);
+    }
+
+    public void removeSpell(int position) {
+        String spell = getSpellList().get(position);
+        int value = spells.get(spell) - 1;
+        if(value == 0){
+            spells.remove(spell);
+        }else {
+            spells.put(spell, value);
+        }
+    }
+
+    public Map<String, Integer> getSpells() {
+        return spells;
+    }
+
+    public int getSpellListSize() {
+        int size = 0;
+        for (String spell: getSpellList()) {
+            Integer value = spells.get(spell);
+            if(value !=null ){
+                size+=value;
+            }else{
+                size+=1;
+            }
+        }
+        return size;
+    }
 }
--- src/pt/joaomneto/titancompanion/adventurecreation/impl/fragments/tcoc/TCOCAdventureCreationSpellsFragment.java
+++ src/pt/joaomneto/titancompanion/adventurecreation/impl/fragments/tcoc/TCOCAdventureCreationSpellsFragment.java
@@ -2,9 +2,14 @@ package pt.joaomneto.titancompanion.adventurecreation.impl.fragments.tcoc;
 
 import pt.joaomneto.titancompanion.R;
 import pt.joaomneto.titancompanion.adventurecreation.impl.TCOCAdventureCreation;
+
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +20,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class TCOCAdventureCreationSpellsFragment extends Fragment {
 
 	public static final int SKILL_POTION = 0;
@@ -23,6 +30,7 @@ public class TCOCAdventureCreationSpellsFragment extends Fragment {
 
 	private TextView spellScoreValue = null;
 	private String[] spellList = null;
+	private TCOCAdventureCreation activity;
 
 	public TCOCAdventureCreationSpellsFragment() {
 
@@ -38,17 +46,16 @@ public class TCOCAdventureCreationSpellsFragment extends Fragment {
 
 		final ListView listview = (ListView) rootView.findViewById(R.id.spellListView);
 
-		final TCOCAdventureCreation activity = (TCOCAdventureCreation) getActivity();
+		activity = (TCOCAdventureCreation) getActivity();
 
 		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,
-	            android.R.layout.simple_list_item_1, android.R.id.text1, spellList);
-		
+				android.R.layout.simple_list_item_1, android.R.id.text1, spellList);
+
 		listview.setAdapter(adapter);
 
 		final ListView selectedSpellsListView = (ListView) rootView.findViewById(R.id.selectedSpellListView);
 
-		final ArrayAdapter<String> selectedSpellsAdapter = new ArrayAdapter<String>(activity,
-	            android.R.layout.simple_list_item_1, android.R.id.text1, activity.getSpells());
+		final ArrayAdapter<String> selectedSpellsAdapter = new SpellListAdapter(activity, activity.getSpellList());
 
 		selectedSpellsListView.setAdapter(selectedSpellsAdapter);
 
@@ -56,19 +63,19 @@ public class TCOCAdventureCreationSpellsFragment extends Fragment {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
-				if(activity.getSpellValue() == activity.getSpells().size())
+				if (activity.getSpellValue() == activity.getSpellList().size())
 					return;
-				activity.getSpells().add(spellList[position]);
+				activity.addSpell(spellList[position]);
 				selectedSpellsAdapter.notifyDataSetChanged();
-				spellScoreValue.setText((activity.getSpellValue()-activity.getSpells().size())+"");
+				spellScoreValue.setText((activity.getSpellValue() - activity.getSpellListSize()) + "");
 			}
 		});
-		
+
 		selectedSpellsListView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
+									int arg2, long arg3) {
 				final int position = arg2;
 				AlertDialog.Builder builder = new AlertDialog.Builder(activity);
 				builder.setTitle("Delete spell?")
@@ -79,17 +86,17 @@ public class TCOCAdventureCreationSpellsFragment extends Fragment {
 										dialog.cancel();
 									}
 								});
-						builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int which) {
-								activity.getSpells().remove(position);
-								selectedSpellsAdapter.notifyDataSetChanged();
-								spellScoreValue.setText((activity.getSpellValue()-activity.getSpells().size())+"");
-							}
-						});
-						
+				builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						activity.removeSpell(position);
+						selectedSpellsAdapter.notifyDataSetChanged();
+						spellScoreValue.setText((activity.getSpellValue() - activity.getSpellList().size()) + "");
+					}
+				});
+
 				AlertDialog alert = builder.create();
 				alert.show();
-				
+
 			}
 		});
 
@@ -98,6 +105,38 @@ public class TCOCAdventureCreationSpellsFragment extends Fragment {
 
 	public TextView getSpellScoreValue() {
 		return spellScoreValue;
+	}
+
+	public class SpellListAdapter extends ArrayAdapter<String> {
+
+
+		public SpellListAdapter(@NonNull Context context, @NonNull List<String> objects) {
+			super(context, -1, objects);
+		}
+
+		@NonNull
+		@Override
+		public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			TCOCAdventureCreation adv = (TCOCAdventureCreation) getContext();
+
+			final View view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+
+			final TextView text1 = (TextView) view.getRootView().findViewById(android.R.id.text1);
+
+			String text = adv.getSpellList().get(position);
+
+			Integer number = adv.getSpells().get(text);
+			if (number != null && number > 1) {
+				text += " (" + number + ")";
+			}
+
+			text1.setText(text);
+
+			return view;
+		}
+
 	}
 
 }
