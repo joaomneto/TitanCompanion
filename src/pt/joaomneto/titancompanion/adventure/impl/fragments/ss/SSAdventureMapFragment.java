4c9c3e7293fd69076728283e4d3a4bfb270b6331
Joao Neto <joao.m.neto@gmail.com>
2017-05-13 14:57:24 +0100

    0.7-alpha
    - Fixed map screen for Scorpion Swamp

--- src/pt/joaomneto/titancompanion/adventure/impl/fragments/ss/SSAdventureMapFragment.java
+++ src/pt/joaomneto/titancompanion/adventure/impl/fragments/ss/SSAdventureMapFragment.java
@@ -7,111 +7,190 @@ import java.util.List;
 import pt.joaomneto.titancompanion.R;
 import pt.joaomneto.titancompanion.adventure.AdventureFragment;
 import pt.joaomneto.titancompanion.adventure.impl.SSAdventure;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SSAdventureMapFragment extends AdventureFragment {
 
-	static List<String> elements = new ArrayList<String>();
-	View rootView;
+    static List<String> elements = new ArrayList<String>();
+    private SSAdventure adv;
+    View rootView;
 
-	static {
-		Class<R.id> clazz = R.id.class;
+    static {
+        Class<R.id> clazz = R.id.class;
 
-		for (Field field : clazz.getFields()) {
-			if (field.getName().startsWith("clearing")) {
-				elements.add(field.getName());
-			}
-			if (field.getName().startsWith("up") || field.getName().startsWith("down")
-					|| field.getName().startsWith("left") || field.getName().startsWith("right")) {
-				elements.add(field.getName());
-			}
-		}
-	}
+        for (Field field : clazz.getFields()) {
+            if (field.getName().startsWith("clearing")) {
+                elements.add(field.getName());
+            }
+            if (field.getName().startsWith("up") || field.getName().startsWith("down")
+                    || field.getName().startsWith("left") || field.getName().startsWith("right")) {
+                elements.add(field.getName());
+            }
+        }
+    }
 
-	Button addClearingButton;
+    Button addClearingButton;
 
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		rootView = inflater.inflate(R.layout.fragment_08st_adventure_map, container, false);
+        super.onCreate(savedInstanceState);
+        rootView = inflater.inflate(R.layout.fragment_08st_adventure_map, container, false);
 
-		final SSAdventure adv = (SSAdventure) getActivity();
+        adv = (SSAdventure) this.getContext();
 
-		addClearingButton = (Button) rootView.findViewById(R.id.addClearingButton);
+        rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                rootView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                setClearingLayoutSizes(rootView, adv);
+            }
+        });
 
-		addClearingButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        final SSAdventure adv = (SSAdventure) getActivity();
 
-				alert.setTitle("Current clearing?");
+        addClearingButton = (Button) rootView.findViewById(R.id.addClearingButton);
 
-				// Set an EditText view to get user input
-				final EditText input = new EditText(getActivity());
-				final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(
-						Context.INPUT_METHOD_SERVICE);
-				imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
-				input.setInputType(InputType.TYPE_CLASS_NUMBER);
-				input.requestFocus();
-				alert.setView(input);
+        addClearingButton.setOnClickListener(new OnClickListener() {
 
-				alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int whichButton) {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
 
-						imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
-						String number = input.getText().toString();
-						adv.addVisitedClearings(number);
-						refreshScreensFromResume();
+                alert.setTitle("Current clearing?");
 
-					}
-				});
+                // Set an EditText view to get user input
+                final EditText input = new EditText(getActivity());
+                final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(
+                        Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+                input.setInputType(InputType.TYPE_CLASS_PHONE);
+                input.requestFocus();
+                alert.setView(input);
 
-				alert.show();
-			}
-		});
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
 
-		refreshScreensFromResume();
+                        imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
+                        String number = input.getText().toString();
+                        adv.addVisitedClearings(number);
+                        refreshScreensFromResume();
 
-		return rootView;
-	}
+                    }
+                });
 
-	@Override
-	public void refreshScreensFromResume() {
+                alert.show();
+            }
+        });
 
-		List<String> showElements = new ArrayList<String>();
+        refreshScreensFromResume();
 
-		SSAdventure adv = (SSAdventure) getActivity();
+        return rootView;
+    }
 
-		for (String number : adv.getVisitedClearings()) {
-			for (String link : elements) {
-				if (link.endsWith("_" + number)) {
-					showElements.add(link);
-				}
-			}
-			Class<R.id> clazz = R.id.class;
-			for (String field : showElements) {
-				try {
-					Field f = clazz.getField(field);
-					int id = f.getInt(null);
-					TextView currcell = (TextView) rootView.findViewById(id);
-					currcell.setVisibility(View.VISIBLE);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    @Override
+    public void refreshScreensFromResume() {
+
+        List<String> showElements = new ArrayList<String>();
+
+        SSAdventure adv = (SSAdventure) getActivity();
+
+        Class<R.id> clazz = R.id.class;
+        for (String number : adv.getVisitedClearings()) {
+            try {
+                revealField(clazz, "clearing_" + number);
+
+            } catch (Exception e) {
+                throw (new RuntimeException(e));
+            }
+        }
+
+
+        if (rootView.findViewById(R.id.clearing_17).getVisibility() == View.VISIBLE
+                && rootView.findViewById(R.id.clearing_24).getVisibility() == View.VISIBLE) {
+            rootView.findViewById(R.id.clearing_18_24).setVisibility(View.VISIBLE);
+        }
+
+
+        if (rootView.findViewById(R.id.clearing_29).getVisibility() == View.VISIBLE
+                && rootView.findViewById(R.id.clearing_33).getVisibility() == View.VISIBLE) {
+            rootView.findViewById(R.id.clearing_29_33).setVisibility(View.VISIBLE);
+        }
+
+
+        if (rootView.findViewById(R.id.clearing_10).getVisibility() == View.VISIBLE
+                && rootView.findViewById(R.id.clearing_28).getVisibility() == View.VISIBLE) {
+            rootView.findViewById(R.id.clearing_28_10).setVisibility(View.VISIBLE);
+            rootView.findViewById(R.id.clearing_10_w).setVisibility(View.VISIBLE);
+        }
+
+        int visibles = 0;
+
+        visibles+=rootView.findViewById(R.id.clearing_7).getVisibility() == View.VISIBLE?1:0;
+        visibles+=rootView.findViewById(R.id.clearing_15).getVisibility() == View.VISIBLE?1:0;
+        visibles+=rootView.findViewById(R.id.clearing_19).getVisibility() == View.VISIBLE?1:0;
+        visibles+=rootView.findViewById(R.id.clearing_32).getVisibility() == View.VISIBLE?1:0;
+
+        if(visibles>1){
+            rootView.findViewById(R.id.clearing_7_15_19_32).setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void revealField(Class<R.id> clazz, String field) throws NoSuchFieldException, IllegalAccessException {
+        Field f = clazz.getField(field);
+        int id = f.getInt(null);
+        TextView currcell = (TextView) rootView.findViewById(id);
+        currcell.setVisibility(View.VISIBLE);
+    }
+
+    public void setClearingLayoutSizes(View rootView, Context context) {
+
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+
+        dpHeight -= addClearingButton.getHeight();
+
+        dpHeight /= 9;
+        dpWidth /= 7;
+
+
+        int finalValue = (int) Math.min(dpHeight, dpWidth);
+
+        TableLayout table = (TableLayout) rootView.findViewById(R.id.clearingGrid);
+        final int childcount = table.getChildCount();
+        for (int i = 0; i < childcount; i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+
+            final int cellCount = row.getChildCount();
+            for (int j = 0; j < cellCount; j++) {
+                TextView cell = (TextView) row.getChildAt(j);
+                cell.setHeight(finalValue);
+                cell.setWidth(finalValue);
+            }
+
+
+        }
+    }
 }
