49ed4f593fbf883821bd1ee34b634b58c9255dfe
joaomneto <joao.m.neto@gmail.com>
2017-05-29 17:15:54 +0100

    Building preferences screen

--- src/pt/joaomneto/titancompanion/TCPreferenceActivity.java
+++ src/pt/joaomneto/titancompanion/TCPreferenceActivity.java
@@ -1,19 +1,15 @@
 package pt.joaomneto.titancompanion;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.preference.PreferenceActivity;
-import android.support.annotation.Nullable;
 
 import java.util.List;
 import java.util.Locale;
 
 import pt.joaomneto.titancompanion.fragment.TCPreferenceFragment;
-import pt.joaomneto.titancompanion.util.ContextWrapper;
+import pt.joaomneto.titancompanion.util.TCContextWrapper;
 
 /**
  * Created by joao on 27/05/17.
@@ -36,19 +32,41 @@ public class TCPreferenceActivity extends PreferenceActivity implements SharedPr
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals("lang")) {
-            String lang = sharedPreferences.getString("lang", "catala");
-            if ("en".equals(lang)) {
-                lang = "en_US";
-            } else {
-                lang = "pt_PT";
-            }
 
-            Locale locale = new Locale(lang);
-                    getBaseContext().getResources().getDisplayMetrics());
-            Context context = ContextWrapper.wrap(this, locale);
-            super.attachBaseContext(context);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("lang", sharedPreferences.getString(key, "en_US"));
+            editor.commit();
+
+            settings();
         }
     }
 
+    public void settings() {
+        Intent intent = new Intent(this, TCPreferenceActivity.class);
+        intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, TCPreferenceFragment.class.getName());
+        intent.putExtra(PreferenceActivity.EXTRA_NO_HEADERS, true);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+
+        SharedPreferences pref =  newBase.getSharedPreferences("lang", MODE_PRIVATE);
+
+        String lang = pref.getString("lang", null);
+
+        if(lang == null){
+
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("lang", "en_US");
+            editor.commit();
+        }
+
+        Locale locale = new Locale(lang);
+
+        Context context = TCContextWrapper.wrap(newBase, locale);
+        super.attachBaseContext(newBase);
+    }
 
 }
