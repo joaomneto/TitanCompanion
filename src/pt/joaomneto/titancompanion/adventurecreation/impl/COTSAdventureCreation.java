5d8b4969876315c3f2fe6b351bd65b23ccdb5aa1
joaomneto <joao.m.neto@gmail.com>
2017-05-30 16:42:18 +0100

    Added support for Crypt of the Sorcerer

---
+++ src/pt/joaomneto/titancompanion/adventurecreation/impl/COTSAdventureCreation.java
@@ -0,0 +1,14 @@
+package pt.joaomneto.titancompanion.adventurecreation.impl;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class COTSAdventureCreation extends BaseAdventureCreation {
+
+
+	@Override
+	protected void storeAdventureSpecificValuesInFile(BufferedWriter bw) throws IOException {
+		bw.write("gold=0\n");
+	}
+
+}
