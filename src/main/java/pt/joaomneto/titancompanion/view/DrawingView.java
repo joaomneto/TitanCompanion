061a47ee7b3cee7e9e1601c6bd55878eb25c6345
Joao Neto <joao.m.neto@gmail.com>
2017-08-12 21:11:38 +0100

    Adding map drawing screen

---
+++ src/main/java/pt/joaomneto/titancompanion/view/DrawingView.java
@@ -0,0 +1,18 @@
+package pt.joaomneto.titancompanion.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DrawingView extends View {
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing() {
+//get drawing area setup for interaction
+    }
+
+}
