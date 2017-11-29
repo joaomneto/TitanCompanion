c63dcbc83b7de53fc7f8ecaecbff989517dc2798
Joao Neto <joao.neto@talkdesk.com>
2017-11-29 14:43:35 +0000

    Add specific SL stats creation screen

--- src/main/java/pt/joaomneto/titancompanion/adventurecreation/impl/SLAdventureCreation.kt
+++ src/main/java/pt/joaomneto/titancompanion/adventurecreation/impl/SLAdventureCreation.kt
@@ -4,6 +4,7 @@ import android.view.View
 import pt.joaomneto.titancompanion.R
 import pt.joaomneto.titancompanion.adventure.Adventure.AdventureFragmentRunner
 import pt.joaomneto.titancompanion.adventurecreation.AdventureCreation
+import pt.joaomneto.titancompanion.adventurecreation.impl.fragments.sl.SLVitalStatisticsFragment
 import pt.joaomneto.titancompanion.util.DiceRoller
 import java.io.BufferedWriter
 import java.io.IOException
@@ -15,8 +16,8 @@ class SLAdventureCreation : AdventureCreation() {
     init {
         AdventureCreation.fragmentConfiguration.clear()
         AdventureCreation.fragmentConfiguration.put(0, AdventureFragmentRunner(
-                R.string.title_adventure_creation_vitalstats,
-                "pt.joaomneto.titancompanion.adventurecreation.impl.fragments.VitalStatisticsFragment"))
+            R.string.title_adventure_creation_vitalstats,
+            "pt.joaomneto.titancompanion.adventurecreation.impl.fragments.sl.SLVitalStatisticsFragment"))
 
     }
 
@@ -34,8 +35,13 @@ class SLAdventureCreation : AdventureCreation() {
         bw.write("starspray=true")
     }
 
+    private fun getSLVitalStatisticsFragment(): SLVitalStatisticsFragment {
+        return getFragments()[0] as SLVitalStatisticsFragment
+    }
+
     override fun rollGamebookSpecificStats(view: View) {
         rating = DiceRoller.rollD6()
+        getSLVitalStatisticsFragment().getRatingValue()?.text = rating.toString()
     }
 
     override fun validateCreationSpecificParameters(): String? {
