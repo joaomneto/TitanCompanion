e07f1c65e862787ec91111d7d9e36fdeaf266d96
Joao Neto <joao.m.neto@gmail.com>
2018-05-16 21:12:05 +0100

    Add Smoke Oil to GOD

--- src/main/java/pt/joaomneto/titancompanion/adventure/impl/GODAdventure.kt
+++ src/main/java/pt/joaomneto/titancompanion/adventure/impl/GODAdventure.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.Menu
 import pt.joaomneto.titancompanion.R
 import pt.joaomneto.titancompanion.adventure.Adventure
-import pt.joaomneto.titancompanion.adventure.impl.fragments.AdventureCombatFragment
-import pt.joaomneto.titancompanion.adventure.impl.fragments.AdventureEquipmentFragment
 import pt.joaomneto.titancompanion.adventure.impl.fragments.AdventureNotesFragment
-import pt.joaomneto.titancompanion.adventure.impl.fragments.AdventureVitalStatsFragment
 import pt.joaomneto.titancompanion.adventure.impl.fragments.god.GODAdventureCombatFragment
 import pt.joaomneto.titancompanion.adventure.impl.fragments.god.GODAdventureEquipmentFragment
+import pt.joaomneto.titancompanion.adventure.impl.fragments.god.GODAdventureVitalStatsFragment
 import pt.joaomneto.titancompanion.adventure.impl.fragments.god.GODWeapon
 import pt.joaomneto.titancompanion.util.AdventureFragmentRunner
 import java.io.BufferedWriter
@@ -17,7 +15,7 @@ import java.io.IOException
 
 class GODAdventure : Adventure(
     arrayOf(
-        AdventureFragmentRunner(R.string.vitalStats, AdventureVitalStatsFragment::class),
+        AdventureFragmentRunner(R.string.vitalStats, GODAdventureVitalStatsFragment::class),
         AdventureFragmentRunner(R.string.fights, GODAdventureCombatFragment::class),
         AdventureFragmentRunner(R.string.goldEquipment, GODAdventureEquipmentFragment::class),
         AdventureFragmentRunner(R.string.notes, AdventureNotesFragment::class)
@@ -26,6 +24,7 @@ class GODAdventure : Adventure(
 
     var weapon: GODWeapon = GODWeapon.UNARMED
     var poison: Int = 0
+    var smokeOil: Int = 0
 
     override val currencyName = R.string.gold
 
@@ -48,11 +47,13 @@ class GODAdventure : Adventure(
         bw.write("gold=$gold\n")
         bw.write("weapon=${weapon.name}\n")
         bw.write("poison=$poison\n")
+        bw.write("smokeOil=$smokeOil\n")
     }
 
     override fun loadAdventureSpecificValuesFromFile() {
         gold = Integer.valueOf(savedGame.getProperty("gold"))
         poison = Integer.valueOf(savedGame.getProperty("poison"))
         weapon = GODWeapon.valueOf(savedGame.getProperty("weapon"))
+        smokeOil = Integer.valueOf(savedGame.getProperty("smokeOil"))
     }
 }
